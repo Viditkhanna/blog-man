a67f7632984e9f0da7ad62804dc36fa6e4e69766
Viditkhanna <viditkhanaa@gmail.com>
2021-03-25 22:48:42 +0530

    Add Parsing to list of blog

--- lib/repo/blog_repo.dart
+++ lib/repo/blog_repo.dart
@@ -1,9 +1,10 @@
+import 'package:homeward/models/blog.dart';
 import 'package:homeward/network/api_client.dart';
 import 'package:homeward/network/api_endpoints.dart';
 
 class BlogRepo {
-  static Future getBlogs() async {
-    var response = await apiClient.get(Api.blogs);
-    return response;
+  static Future<List<Blog>> getBlogs() async {
+    var response = await apiClient.get(Api.blogs) as List;
+    return response.map((e) => Blog.fromMap(e)).toList();
   }
 }

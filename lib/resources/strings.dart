13046d955359754ea9f497ff1ba0f655e95f419e
Viditkhanna <viditkhanaa@gmail.com>
2021-03-25 22:36:58 +0530

    Add strings

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -4,6 +4,7 @@ class Strings {
   static const login = 'Login';
   static const error = 'Error';
   static const ok = 'Ok';
+  static const blogs = 'Blogs';
   static const invalidEmailAlert = 'Please enter a valid email';
   static const invalidPasswordAlert =
       'Please enter a password with more than 5 characters';

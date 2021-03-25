6dfc9a019b69f3ae1f1b778e68d81935b1845e4e
Viditkhanna <viditkhanaa@gmail.com>
2021-03-25 22:37:50 +0530

    Add Auto-Navigate to bloglist when logged-in
    Check if login through sharef-prefs

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:homeward/data/shared_prefs.dart';
 import 'package:homeward/ui/auth/login.dart';
+import 'package:homeward/ui/blog/blog_list.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: LoginPage(),
+      home: FutureBuilder(
+          future: Prefs.getToken(),
+          builder: (context, snap) {
+            if (snap.connectionState == ConnectionState.waiting)
+              return Container();
+            if (snap.hasData) return BlogList();
+            return LoginPage();
+          }),
     );
   }
 }

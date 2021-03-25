22ae1407f884eecce9ff55f7ad13007626ac4c84
Viditkhanna <viditkhanaa@gmail.com>
2021-03-25 21:10:03 +0530

    Add controllers and validators

--- lib/ui/auth/login.dart
+++ lib/ui/auth/login.dart
@@ -1,34 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:homeward/resources/strings.dart';
+import 'package:regexpattern/regexpattern.dart';
 
 class LoginPage extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
+
+  final emailCtrl = TextEditingController();
+  final pwdCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text(Strings.login),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            child: TextFormField(
-              decoration: InputDecoration(labelText: 'Email'),
+      body: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: TextFormField(
+                controller: emailCtrl,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) =>
+                    value.isEmail() ? null : Strings.invalidEmailAlert,
+                decoration: InputDecoration(labelText: Strings.email),
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: TextFormField(
+                controller: pwdCtrl,
+                validator: (value) =>
+                    value.length > 5 ? null : Strings.invalidPasswordAlert,
+                obscureText: true,
+                decoration: InputDecoration(labelText: Strings.password),
+              ),
             ),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('Login'),
-          )
-        ],
+            RaisedButton(
+              onPressed: () {
+                if (!formKey.currentState.validate()) return;
+              },
+              child: Text(Strings.login),
+            )
+          ],
+        ),
       ),
     );
   }

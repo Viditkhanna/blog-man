1031222b5795bdf55fc9d2757292cc4f29e0476c
Viditkhanna <viditkhanaa@gmail.com>
2021-03-25 21:25:00 +0530

    Add auth-repo class to hit the login API

---
+++ lib/repo/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:homeward/network/api_client.dart';
+import 'package:homeward/network/api_endpoints.dart';
+
+class AuthRepo {
+  static Future login(String email, String password) async {
+    var response = await apiClient.post(Api.login, {
+      'email': email,
+      'password': password,
+    });
+  }
+}

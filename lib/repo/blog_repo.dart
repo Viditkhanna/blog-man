03020455c7263a08215e764cd0d27ac6b144ed2a
Viditkhanna <viditkhanaa@gmail.com>
2021-03-26 23:31:50 +0530

    Add api function to get blog details from API

--- lib/repo/blog_repo.dart
+++ lib/repo/blog_repo.dart
@@ -1,4 +1,5 @@
 import 'package:homeward/models/blog.dart';
+import 'package:homeward/models/blog_details.dart';
 import 'package:homeward/network/api_client.dart';
 import 'package:homeward/network/api_endpoints.dart';
 
@@ -6,5 +7,10 @@ class BlogRepo {
   static Future<List<Blog>> getBlogs() async {
     var response = await apiClient.get(Api.blogs) as List;
     return response.map((e) => Blog.fromMap(e)).toList();
+  }
+
+  static Future<BlogDetails> getBlogDetails(String id) async {
+    var response = await apiClient.get(Api.blogs + '/$id');
+    return BlogDetails.fromMap(response);
   }
 }

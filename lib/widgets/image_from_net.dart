84ae0ef3cefb5d42818510c434aa3e0d286e8fb5
Viditkhanna <viditkhanaa@gmail.com>
2021-03-26 23:32:23 +0530

    Add parameter width and height for image

--- lib/widgets/image_from_net.dart
+++ lib/widgets/image_from_net.dart
@@ -3,21 +3,27 @@ import 'package:flutter/material.dart';
 
 class ImageFromNet extends StatelessWidget {
   final String imageUrl;
+  final height;
+  final width;
 
   const ImageFromNet({
     Key key,
     @required this.imageUrl,
+    this.height,
+    this.width,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       imageUrl: imageUrl,
+      height: height,
+      width: width,
       errorWidget: (context, error, stackTrace) {
         return Icon(Icons.error);
       },
       placeholder: (context, url) {
-        return CircularProgressIndicator();
+        return Center(child: CircularProgressIndicator());
       },
     );
   }
